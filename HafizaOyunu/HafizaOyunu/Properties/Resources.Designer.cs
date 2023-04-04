600fbc9b14ce24996f7f28b11c4b9e4eb5b15dd7
Tuba Yilmaz <tubayilmaz1532gmail.com>
2023-04-05 02:38:22 +0300

    updated

--- HafizaOyunu/HafizaOyunu/Properties/Resources.Designer.cs
+++ HafizaOyunu/HafizaOyunu/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace sinirkrizi.Properties {
+namespace hafiza.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace sinirkrizi.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("sinirkrizi.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("hafiza.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
